7fdc3c2cf1883c528a678d055db930588f739b68
vedroidek <momon_boton@mail.ru>
2024-04-22 16:09:40 +0500

    Minor changes

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 
+from app.core import errors
+
 
 bp_api = Blueprint('api', __name__, url_prefix='/api/v0.1')
 
-from app.api import users, errors, tokens, api_info
+from app.api import users, tokens, api_info
